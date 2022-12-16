b2f37ad69c750220729d6572ec3901c7a07f0d0b
Egor Kisel <kisel1989@mail.ru>
2022-12-16 05:55:05 +0300

    activity_EditTextAction

--- app/src/test/java/com/geekbrains/myfirsttests/MainActivityTest.kt
+++ app/src/test/java/com/geekbrains/myfirsttests/MainActivityTest.kt
@@ -2,6 +2,7 @@ package com.geekbrains.myfirsttests
 
 import android.content.Context
 import android.os.Build
+import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.TextView
 import androidx.test.core.app.ActivityScenario
@@ -17,6 +18,7 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
+import org.robolectric.shadows.ShadowToast
 
 @RunWith(AndroidJUnit4::class)
 @Config(sdk = [Build.VERSION_CODES.O_MR1])
@@ -43,6 +45,15 @@ class MainActivityTest {
             editText.setText("text", TextView.BufferType.EDITABLE)
             assertNotNull(editText.text)
             assertEquals("text", editText.text.toString())
+        }
+    }
+
+    @Test
+    fun activity_EditTextAction() {
+        scenario.onActivity {
+            val editText = it.findViewById<EditText>(R.id.searchEditText)
+            editText.onEditorAction(EditorInfo.IME_ACTION_SEARCH)
+            assertEquals(ShadowToast.getTextOfLatestToast(), "Enter search word")
         }
     }
 }
