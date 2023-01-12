581801de87570965039a864d7440975f0abb9394
Egor Kisel <kisel1989@mail.ru>
2023-01-12 09:44:54 +0300

    DetailsFragmentEspressoTest

--- app/src/androidTest/java/com/geekbrains/myfirsttests/SharedTestData.kt
+++ app/src/androidTest/java/com/geekbrains/myfirsttests/SharedTestData.kt
@@ -7,4 +7,3 @@ internal const val TEST_NUMBER_OF_RESULTS_MINUS_1 = "Number of results: -1"
 internal const val TEST_RESPONSE_IS_NULL_OR_UNSUCCESSFUL = "Response is null or unsuccessful"
 internal const val TEST_SEARCH_RESULTS_OR_TOTAL_COUNT_ARE_NULL = "Search results or total count are null"
 internal const val SOME_QUERY = "some query"
-
